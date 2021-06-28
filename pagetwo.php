3147acf35836148b5f4af28df3d15a3427216a07
Maksim Myheev <m.myheev@gmail.com>
2021-06-28 05:11:26 +0300

    feat: added list of diagnoses

--- pagetwo.php
+++ pagetwo.php
@@ -4,15 +4,36 @@
 
 <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Страница 2</title>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="style.css">
+        <title>Страница 2</title>
+    </head>
 
-<body>
+    <body>
 
-</body>
+        <div class="top-menu">
+
+            <ul>
+            <li><a href="index.php">Список пациентов</a></li>
+            <li><a href="pagetwo.php">Инциденты</a></li>
+            </ul>
+
+        </div>
+
+        <table>
+            <tr>
+                <th><a>UID</a></th>
+                <th>Пациент</th>
+                <th>Диагноз</th>
+                <th>Дата открытия</th>
+                <th>Дата закрытия</th>
+                <th></th>
+            </tr>
+        </table>
+
+    </body>
 
 </html>
